e5d8b9a5cc0c5d77371ded1c41194ba3704112e4
Hendrikus Adi Purnama <hendrikus.purnama@ptgue.com>
2022-03-27 13:48:31 +0700

    [ @hendrikusray ] - [ cms ] - [msg: init project ]

---
+++ src/component/Page.jsx
@@ -0,0 +1,24 @@
+import logo from './../logo.svg';
+import '../assets/Page.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span>halo guys</span>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
